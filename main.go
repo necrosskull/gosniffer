7c82419ca75d7775c27930e52c2cba59b1365b58
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2024-03-31 20:36:41 +0300

    update: decompose

--- main.go
+++ main.go
@@ -27,11 +27,37 @@ var (
 
 var packetBuffer []gopacket.Packet
 
-var device string = ""
+func init() {
+	flag.Parse()
+}
 
 func main() {
-	flag.Parse()
+	if *iface == "" {
+		log.Fatal("Укажите имя интерфейса --i параметр")
+	}
 
+	netIP := getNetIP()
+	device := findDevice(netIP)
+
+	validateSaveDir()
+
+	timeout := getTimeout()
+	handle := openPcapHandle(device, timeout)
+
+	applyFilter(handle)
+
+	log.Printf("Прослушивается Интерфейс: %s, IP: %s", *iface, netIP)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go handleSignal(sigs, handle)
+
+	pkgsrc := gopacket.NewPacketSource(handle, handle.LinkType())
+	capturePackets(pkgsrc)
+}
+
+func validateSaveDir() {
 	_, err := os.Stat(*saveDir)
 	if os.IsNotExist(err) {
 		errDir := os.MkdirAll(*saveDir, 0755)
@@ -44,22 +70,19 @@ func main() {
 		log.Fatal("Ошибка при проверке папки: ", err)
 		return
 	}
+}
 
-	var timeout time.Duration
-
+func getTimeout() time.Duration {
 	if *timeoutT == 0 {
-		timeout = -1
-	} else {
-		timeout = time.Duration(*timeoutT) * time.Second
+		return -1
 	}
+	return time.Duration(*timeoutT) * time.Second
+}
 
-	if *iface == "" {
-		log.Fatal("Укажите имя интерфейса --i параметр")
-	}
-
+func getNetIP() net.IP {
 	netName, err := net.InterfaceByName(*iface)
 	if err != nil {
-		log.Fatalf("InterfaceByName: %v", err)
+		log.Fatalf("InterfaceByName: %v: %v", err, *iface)
 	}
 	netAddrs, err := netName.Addrs()
 	if err != nil {
@@ -69,81 +92,81 @@ func main() {
 	if err != nil {
 		log.Fatalf("ParseCIDR: %v", err)
 	}
+	return netIP
+}
 
-	log.Printf("Прослушивается Интерфейс: %s, IP: %s", *iface, netIP)
-
+func findDevice(netIP net.IP) string {
 	allInterfaces, err := pcap.FindAllDevs()
 	if err != nil {
 		log.Fatalf("FindAllDevs: %v", err)
 	}
 
 	for _, pcapiface := range allInterfaces {
-
 		if len(pcapiface.Addresses) < 1 {
 			continue
 		}
 
 		addr := pcapiface.Addresses[0].IP
-
 		if addr.Equal(netIP) {
-			device = pcapiface.Name
-			break
+			return pcapiface.Name
 		}
-
 	}
 
-	if device == "" {
-		log.Fatalf("Не найден интерфейс %v", *iface)
+	log.Fatalf("Не найден интерфейс %v", *iface)
+	return ""
+}
 
-	}
-
+func openPcapHandle(device string, timeout time.Duration) *pcap.Handle {
 	handle, err := pcap.OpenLive(device, int32(*snaplen), *promisc, timeout)
 	if err != nil {
 		log.Fatalf("OpenLive: %v", err)
 	}
-	defer handle.Close()
+	return handle
+}
 
+func applyFilter(handle *pcap.Handle) {
 	if *filter != "" {
 		log.Println("applying filter ", *filter)
 		err := handle.SetBPFFilter(*filter)
 		if err != nil {
 			log.Fatal(err)
+		}
+	}
+}
 
+func handleSignal(sigs chan os.Signal, handle *pcap.Handle) {
+	<-sigs
+	savePcapFile(handle)
+	os.Exit(0)
+}
+
+func savePcapFile(handle *pcap.Handle) {
+	fileName := fmt.Sprintf("%s_%s.pcap", *iface, time.Now().Format("2006-01-02_15-04-05"))
+	filePath := filepath.Join(*saveDir, fileName)
+
+	f, err := os.Create(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+	defer handle.Close()
+
+	pcapw := pcapgo.NewWriter(f)
+	if err := pcapw.WriteFileHeader(uint32(*snaplen), handle.LinkType()); err != nil {
+		log.Fatalf("WriteFileHeader: %v", err)
+	}
+
+	for _, packet := range packetBuffer {
+		if err := pcapw.WritePacket(packet.Metadata().CaptureInfo, packet.Data()); err != nil {
+			log.Fatalf("pcap.WritePacket(): %v", err)
 		}
 	}
 
-	pkgsrc := gopacket.NewPacketSource(handle, handle.LinkType())
+	log.Printf("pcap файл создан по пути: %s", filePath)
+}
 
-	sigs := make(chan os.Signal, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		<-sigs
-		fileName := fmt.Sprintf("%s_%s.pcap", *iface, time.Now().Format("2006-01-02_15-04-05"))
-		filePath := filepath.Join(*saveDir, fileName)
-
-		f, err := os.Create(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-
-		pcapw := pcapgo.NewWriter(f)
-		if err := pcapw.WriteFileHeader(uint32(*snaplen), handle.LinkType()); err != nil {
-			log.Fatalf("WriteFileHeader: %v", err)
-		}
-
-		for _, packet := range packetBuffer {
-			if err := pcapw.WritePacket(packet.Metadata().CaptureInfo, packet.Data()); err != nil {
-				log.Fatalf("pcap.WritePacket(): %v", err)
-			}
-		}
-
-		log.Printf("pcap файл создан по пути: %s", filePath)
-		os.Exit(0)
-	}()
-
+func capturePackets(pkgsrc *gopacket.PacketSource) {
 	for packet := range pkgsrc.Packets() {
 		packetBuffer = append(packetBuffer, packet)
 	}
